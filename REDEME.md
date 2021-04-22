ded3b2f68e5504fd170452ba44de415cb64d19ef
yangxueyou <wb-yxy494884@alibaba-inc.com>
2021-04-22 18:32:12 +0800

    first commit

---
+++ REDEME.md
@@ -0,0 +1,18 @@
+# 环境
+1. Nodejs >= 10.16.3 (LTS)
+
+> https://nodejs.org/zh-cn/
+
+2. 安装taiko
+
+> npm install -g taiko
+
+3. 安装gauge
+
+> npm install @getgauge/cli
+
+4. 初始化项目
+
+> gauge init ts
+
+> npm run test
---
+++ tests/xueyou_test.js
@@ -0,0 +1,27 @@
+"use strict";
+const path = require('path');
+const {
+    openBrowser,
+    write,
+    closeBrowser,
+    goto,
+    press,
+    screenshot,
+    above,
+    click,
+    checkBox,
+    listItem,
+    toLeftOf,
+    link,
+    text,
+    into,
+    textBox,
+    evaluate
+} = require('taiko');
+
+step("Complete tasks <table>", async function (table) {
+    for (var row of table.rows) {
+        await click(checkBox(toLeftOf(row.cells[0])));
+    }
+    await gauge.screenshot()
+});
